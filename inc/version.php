e3a066e94a28dfd602dd577eea8533d0307ee9dc
John Elliot V <jj5@jj5.net>
2024-07-01 05:16:17 +1000

    Work, work...

---
+++ inc/version.php
@@ -0,0 +1,15 @@
+<?php
+
+// the version numbers are managed by svnman...
+define( 'MUDBALL_VERSION_MAJOR', 0 );
+define( 'MUDBALL_VERSION_MINOR', 5 );
+define( 'MUDBALL_VERSION_PATCH', 1545 );
+
+// the following are automatically updated by Suybversion...
+define(
+  'MUDBALL_SVN_DATE',
+  '$Date: 2024-07-01 04:44:27 +1000 (Mon, 01 Jul 2024) $'
+);
+define( 'MUDBALL_SVN_REVISION', '$Revision: 10244 $' );
+define( 'MUDBALL_SVN_AUTHOR', '$Author: jj5 $' );
+
