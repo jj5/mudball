7cdd438a4e73b6c83eb955a58248fbc48aceb09f
John Elliot V <jj5@jj5.net>
2024-07-10 16:25:24 +1000

    Work, work...

--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export MUDBALL_NAME=''Mudball: Web Framework and Toolkit for PHP'';
 export MUDBALL_CODE=''mudball'';
 
-export MUDBALL_VERSION='0.5.1575';
+export MUDBALL_VERSION='0.5.1577';
 export MUDBALL_VERSION_MAJOR='0';
 export MUDBALL_VERSION_MINOR='5';
-export MUDBALL_VERSION_PATCH='1575';
+export MUDBALL_VERSION_PATCH='1577';
 
-export MUDBALL_SVN_DATE='Date: 2024-07-09 16:04:43 +1000 (Tue, 09 Jul 2024)';
-export MUDBALL_SVN_REVISION='Revision: 125192378';
+export MUDBALL_SVN_DATE='Date: 2024-07-10 16:25:24 +1000 (Wed, 10 Jul 2024)';
+export MUDBALL_SVN_REVISION='Revision: 67779254';
 export MUDBALL_SVN_AUTHOR='Author: jj5';
 
-export MUDBALL_GIT_DATE='2024-07-09 16:04:43 +1000 (Tue, 09 Jul 2024)';
+export MUDBALL_GIT_DATE='2024-07-10 16:25:24 +1000 (Wed, 10 Jul 2024)';
