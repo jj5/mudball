f4a17c14c4a3f6354f1071fc579156bd571f62e3
John Elliot V <john@blackbrick.com.au>
2024-08-07 12:52:01 +1000

    Work, work...

--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export MUDBALL_NAME=''Mudball: Web Framework and Toolkit for PHP'';
 export MUDBALL_CODE=''mudball'';
 
-export MUDBALL_VERSION='0.5.1637';
+export MUDBALL_VERSION='0.5.1639';
 export MUDBALL_VERSION_MAJOR='0';
 export MUDBALL_VERSION_MINOR='5';
-export MUDBALL_VERSION_PATCH='1637';
+export MUDBALL_VERSION_PATCH='1639';
 
-export MUDBALL_SVN_DATE='Date: 2024-08-07 12:49:47 +1000 (Wed, 07 Aug 2024)';
-export MUDBALL_SVN_REVISION='Revision: 20416581';
+export MUDBALL_SVN_DATE='Date: 2024-08-07 12:52:01 +1000 (Wed, 07 Aug 2024)';
+export MUDBALL_SVN_REVISION='Revision: 113118980';
 export MUDBALL_SVN_AUTHOR='Author: jj5';
 
-export MUDBALL_GIT_DATE='2024-08-07 12:49:47 +1000 (Wed, 07 Aug 2024)';
+export MUDBALL_GIT_DATE='2024-08-07 12:52:01 +1000 (Wed, 07 Aug 2024)';
