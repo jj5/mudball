ca41fc653b89fa0a4378ffed74a8936467a977f0
John Elliot V <jj5@jj5.net>
2024-08-23 20:33:38 +1000

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -6,16 +6,16 @@
 define( 'MUDBALL_NAME', 'Mudball: Web Framework and Toolkit for PHP' );
 define( 'MUDBALL_CODE', 'mudball' );
 
-//define( 'MUDBALL_VERSION', '0.5.1693' );
+//define( 'MUDBALL_VERSION', '0.5.1695' );
 define( 'MUDBALL_VERSION_MAJOR', 0 );
 define( 'MUDBALL_VERSION_MINOR', 5 );
-define( 'MUDBALL_VERSION_PATCH', 1693 );
+define( 'MUDBALL_VERSION_PATCH', 1695 );
 
 define(
   'MUDBALL_SVN_DATE',
-  '$Date: 2024-08-23 14:43:23 +1000 (Fri, 23 Aug 2024) $'
+  '$Date: 2024-08-23 20:33:38 +1000 (Fri, 23 Aug 2024) $'
 );
-define( 'MUDBALL_SVN_REVISION', '$Revision: 64843596 $' );
+define( 'MUDBALL_SVN_REVISION', '$Revision: 47068367 $' );
 define( 'MUDBALL_SVN_AUTHOR', '$Author: jj5 $' );
 
-define( 'MUDBALL_GIT_DATE', '2024-08-23 14:43:23 +1000 (Fri, 23 Aug 2024)' );
+define( 'MUDBALL_GIT_DATE', '2024-08-23 20:33:38 +1000 (Fri, 23 Aug 2024)' );
--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export MUDBALL_NAME=''Mudball: Web Framework and Toolkit for PHP'';
 export MUDBALL_CODE=''mudball'';
 
-export MUDBALL_VERSION='0.5.1693';
+export MUDBALL_VERSION='0.5.1695';
 export MUDBALL_VERSION_MAJOR='0';
 export MUDBALL_VERSION_MINOR='5';
-export MUDBALL_VERSION_PATCH='1693';
+export MUDBALL_VERSION_PATCH='1695';
 
-export MUDBALL_SVN_DATE='Date: 2024-08-23 14:43:23 +1000 (Fri, 23 Aug 2024)';
-export MUDBALL_SVN_REVISION='Revision: 64843596';
+export MUDBALL_SVN_DATE='Date: 2024-08-23 20:33:38 +1000 (Fri, 23 Aug 2024)';
+export MUDBALL_SVN_REVISION='Revision: 47068367';
 export MUDBALL_SVN_AUTHOR='Author: jj5';
 
-export MUDBALL_GIT_DATE='2024-08-23 14:43:23 +1000 (Fri, 23 Aug 2024)';
+export MUDBALL_GIT_DATE='2024-08-23 20:33:38 +1000 (Fri, 23 Aug 2024)';
